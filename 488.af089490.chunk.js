6af43269f667bd494fbcc4cc71007b938aec7527
aiymbaimagulova-beep <aiym.bainagulova@gmail.com>
2026-04-03 01:36:33 +0500

    только нужные файлы

--- 488.af089490.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(globalThis.webpackChunkivory_sins=globalThis.webpackChunkivory_sins||[]).push([[488],{488(s,e,i){new WeakMap;new Set}}]);
-//# sourceMappingURL=488.af089490.chunk.js.map
